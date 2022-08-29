0a2923af993a43b8b723529c28d4317889e999aa
JHA Ravi Kant <RJA3@dsone.3ds.com>
2022-08-29 22:37:43 +0530

    implemented searching of phrase by character

---
+++ src/components/Phrases/Phrase.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card elevation="2">
+    <h2>Phrase by {{ characterName }}</h2>
+    <p>{{ phrase.phrase }}</p></v-card
+  >
+</template>
+
+<script>
+import charactersObject from "../../data/characters.json";
+export default {
+  name: "Phrase",
+  props: ["phrase"],
+  data() {
+    return {};
+  },
+  computed: {
+    getAllCharacters() {
+      return charactersObject.data;
+    },
+
+    getChracterById() {
+      return this.getAllCharacters.find((c) => c._id === this.phrase.character);
+    },
+
+    characterName() {
+      return `${this.getChracterById.firstName} ${this.getChracterById.lastName}`;
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <app-phrases></app-phrases>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import Phrases from '../components/Phrases/Phrases.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+
+    components: {
+      'app-phrases': Phrases
+    },
   }
-}
 </script>
